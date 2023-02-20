847157b5df73b9e7ac19446eb99afa4703ad27f8
Dias.Murilo <dias.murilo@gmail.com>
2023-02-20 14:26:25 +0000

    sync

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,13 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { GUI } from 'dat.gui'
+import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -13,42 +15,61 @@ const camera = new THREE.PerspectiveCamera(
     50,
     window.innerWidth / window.innerHeight,
     0.1,
-    2000
+    5000
 )
 
-const renderer = new THREE.WebGLRenderer({antialias: true})
-renderer.physicallyCorrectLights = true
-renderer.shadowMap.enabled = true
-renderer.setSize(window.innerWidth, window.innerHeight)
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.outputEncoding = THREE.sRGBEncoding
+renderer.shadowMap.enabled = true
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+renderer.physicallyCorrectLights = true
 
+//resize view as user change windows size
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+}
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 0, 0)
 
 let currentUrl = window.location.href;
 let file = currentUrl.split('/').pop()
 let ext = file?.split('.').pop()
-let url = 'models/'+ file
+let url = 'models/' + file
 var boxSize: number[] = new Array()
-const absMaterial = new THREE.MeshNormalMaterial
-switch(ext){
+// const absMaterial = new THREE.MeshNormalMaterial
+const absMaterial = new THREE.MeshPhongMaterial({
+    color: 0x1111111,
+    wireframe: false,
+    flatShading: false,
+    transparent: false
+})
+
+var isCustomMaterial = false;
+
+switch (ext) {
     case 'fbx':
 
-        const fbxLoader = new FBXLoader()
+        const fbxLoader = new FBXLoader();
         fbxLoader.load(
             url,
-            (object) => {
-                object.traverse(function (child) {
-                    if ((child as THREE.Mesh).isMesh) {
-                        (child as THREE.Mesh).material = absMaterial
-                        if ((child as THREE.Mesh).material) {
-                            ((child as THREE.Mesh).material as THREE.MeshNormalMaterial).transparent = false
-                        }
-                    }
-                })
-                guiInitializer(absMaterial)
-                scene.add(object)
-                boxSize = getBoxSize(object)
-                setCamera(boxSize)
+            (fbx) => {
+                // fbx.traverse(function (child) {
+                //     if ((child as THREE.Mesh).isMesh) {
+                //         (child as THREE.Mesh).material = absMaterial
+                //         materialConfig(child)
+                //     }
+                // })
+                materialConfig(fbx);
+                renderObject(fbx);
+                guiInitializer(absMaterial, fbx);
             },
             (xhr) => {
                 console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -58,30 +79,29 @@ switch(ext){
             }
         )
         break;
-    
+
     case 'stl':
         const stlLoader = new STLLoader()
-        stlLoader.load( url, function ( geometry ) {
-            var mesh = new THREE.Mesh( geometry, absMaterial );
-            mesh.position.set( 0, 0, 0 );
-            mesh.rotation.set( - Math.PI / 2, 0, 0 );
-            scene.add( mesh );
+        stlLoader.load(url, function (geometry) {
+            var mesh = new THREE.Mesh(geometry, absMaterial);
+            mesh.position.set(0, 0, 0);
+            mesh.rotation.set(- Math.PI / 2, 0, 0);
+            scene.add(mesh);
             mesh.castShadow = true;
             mesh.receiveShadow = true;
             boxSize = getBoxSize(mesh)
             setCamera(boxSize)
-            guiInitializer(absMaterial)
+            guiInitializer(absMaterial, mesh)
         },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-        },
-        (error) => {
-            console.log(error)
-        });
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            });
         break;
 
     case 'glb':
-        //no break if glb so it will be handled as gltf
     case 'gltf':
         const dracoLoader = new DRACOLoader()
         dracoLoader.setDecoderPath('/js/libs/draco/')
@@ -91,22 +111,22 @@ switch(ext){
         loader.load(
             url,
             function (gltf) {
-                        gltf.scene.traverse(function (child) {
-                            if ((child as THREE.Mesh).isMesh) {
-                                (child as THREE.Mesh).material = absMaterial
-                            }
-                            boxSize = getBoxSize(child)
-                            setCamera(boxSize)
-                        })
-                        guiInitializer(absMaterial)
-                        scene.add(gltf.scene)
-                    },
-                    (xhr) => {
-                        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-                    },
-                    (error) => {
-                        console.log(error)
+                gltf.scene.traverse(function (child) {
+                    if ((child as THREE.Mesh).isMesh) {
+                        (child as THREE.Mesh).material = absMaterial
                     }
+                    boxSize = getBoxSize(child)
+                    setCamera(boxSize)
+                })
+                guiInitializer(absMaterial, gltf.scene)
+                scene.add(gltf.scene)
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            }
         )
         break;
 
@@ -124,7 +144,7 @@ switch(ext){
                 scene.add(object)
                 boxSize = getBoxSize(object)
                 setCamera(boxSize)
-                guiInitializer(absMaterial)
+                guiInitializer(absMaterial, object)
             },
             (xhr) => {
                 console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -133,75 +153,136 @@ switch(ext){
                 console.log(error)
             }
         )
-        break;     
-            
+        break;
+
     default:
         alert('No file found or extension not supported')
 }
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
-
-function setCamera(objectSize: Array<number>){
+function setCamera(objectSize: Array<number>) {
     scene.background = new THREE.Color(0xfffff0)
-    camera.lookAt(new THREE.Vector3(0,0,0))
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
     let x
     let y
     let z
-    if(objectSize[0] != undefined && objectSize[1] != undefined && objectSize[1] != undefined){
+    if (objectSize[0] != undefined && objectSize[1] != undefined && objectSize[1] != undefined) {
         x = objectSize[0]
         y = objectSize[1]
         z = objectSize[2]
     } else {
-        var degree = Math.PI/180;
+        var degree = Math.PI / 180;
         z = 120;
         y = 120;
         x = -45 * degree;
     }
-    camera.position.set(y, x, z+x+y)
+    camera.position.set(y, x, z + x + y)
 
     const light = new THREE.PointLight()
-    light.position.set(x, y, z+x+y)
-    light.position.set(x, y, -(z+x+y))
+    light.position.set(x, y, z + x + y)
+    light.position.set(x, y, -(z + x + y))
     scene.add(light)
 
+    // const ambientLight = new THREE.AmbientLight()
+    // scene.add(ambientLight)
+    var hemiLight = new THREE.HemisphereLight(0x443333, 0x111122, 0.61)
+    hemiLight.position.set(0, 50, 0)
+    scene.add(hemiLight)
+    addShadowedLight(1, -1, 1, 0xffaa00, 1.35)
+    addShadowedLight(1, 1, -1, 0xffaa00, 1)
+    const light1 = new THREE.PointLight()
+    const light2 = new THREE.PointLight()
+    light1.position.set(0, 0, x + y + z)
+    light2.position.set(0, 0, -x - y - z)
+    scene.add(light1)
+    scene.add(light2)
     const ambientLight = new THREE.AmbientLight()
     scene.add(ambientLight)
+    // cameraTarget = new THREE.Vector3(0,0,0)
 
-    scene.add(new THREE.AxesHelper(Math.max(x,y,z)))
+    scene.add(new THREE.AxesHelper(Math.max(x, y, z)))
 }
 
-function getBoxSize(object: THREE.Group | THREE.Object3D<THREE.Event> | THREE.Mesh<THREE.BufferGeometry, THREE.MeshNormalMaterial>){
-    let boundingBox = new THREE.Box3().setFromObject( object );
+function addShadowedLight(x: any, y: any, z: any, color: any, intensity: any) {
+    var directionalLight = new THREE.DirectionalLight(color, intensity);
+    directionalLight.position.set(x, y, z);
+    scene.add(directionalLight);
+    directionalLight.castShadow = true;
+
+    var d = 1;
+    directionalLight.shadow.camera.left = - d;
+    directionalLight.shadow.camera.right = d;
+    directionalLight.shadow.camera.top = d;
+    directionalLight.shadow.camera.bottom = - d;
+    directionalLight.shadow.camera.near = 1;
+    directionalLight.shadow.camera.far = 4;
+    directionalLight.shadow.bias = - 0.002;
+}
+
+function getBoxSize(object: THREE.Group | THREE.Object3D<THREE.Event> | THREE.Mesh<THREE.BufferGeometry, THREE.MeshNormalMaterial>) {
+    let boundingBox = new THREE.Box3().setFromObject(object);
     let boxSize = new THREE.Vector3();
     boundingBox.getSize(boxSize);
-    return [Math.round(boxSize.x),Math.round(boxSize.y),Math.round(boxSize.z)]
+    return [Math.round(boxSize.x), Math.round(boxSize.y), Math.round(boxSize.z)]
 }
 
-//resize view as user change windows size
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.render(scene, camera)
+
+// object.traverse(function (child) {
+//     if ((child as THREE.Mesh).isMesh) {
+//         // (child as THREE.Mesh).material = material
+//         if ((child as THREE.Mesh).material) {
+//             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+//         }
+//     }
+// })
+
+function materialConfig(object){
+    object.traverse(function (child) {
+        if ((child as THREE.Mesh).isMesh) {
+            if (isCustomMaterial == true) {
+                (child as THREE.Mesh).material = absMaterial
+            }
+            // (child as THREE.Mesh).shadow = true;
+            // (child as THREE.Mesh).castShadow = true;
+            // (child as THREE.Mesh).receiveShadow = true;
+        }
+    });
 }
 
-function guiInitializer(material: any){
+function renderObject(fisicObj: any) {
+    scene.add(fisicObj)
+    fisicObj.position.set(0, 0, 0)
+    fisicObj.castShadow = true
+    fisicObj.receiveShadow = true
+    boxSize = getBoxSize(fisicObj)
+    setCamera(boxSize)
+}
+function guiInitializer(material: any, objectTD: any) {
+    const options = {
+        wireframe: false,
+        object: 0x1111111,
+        background: 0xffffff0,
+        lights: true,
+        transparent: true
+    }
+
     const gui = new GUI()
-    const controlFolder = gui.addFolder('Controls')
-    controlFolder.add(material, 'transparent').onChange(() => material.needsUpdate = true)
-    controlFolder.add(material, 'opacity', 0, 1, 0.01)
-    controlFolder.add(material, 'wireframe')
-    controlFolder
-        .add(material, 'flatShading')
-        .onChange(() => updateMaterial(material))
-        controlFolder.open()
-}
+    const controlMaterial = gui.addFolder('Control Material')
+    controlMaterial.add(material, 'transparent').onChange(() => material.needsUpdate = true)
+    controlMaterial.add(material, 'opacity', 0, 1, 0.01)
+    controlMaterial.add(material, 'wireframe').onChange(() => material.needsUpdated = true)
+    controlMaterial.add(material, 'flatShading').onChange(() => material.needsUpdated = true)
+    controlMaterial.add(objectTD, 'visible')
+    controlMaterial.open()
 
-function updateMaterial(material: any) {
-    material.needsUpdate = true
+    const ctrlCamera = gui.addFolder('Control Camera')
+    ctrlCamera.add(objectTD.rotation, 'x', 0, Math.PI * 2)
+    ctrlCamera.add(objectTD.rotation, 'y', 0, Math.PI * 2)
+    ctrlCamera.add(objectTD.rotation, 'z', 0, Math.PI * 2)
+    ctrlCamera.open()
+
+    const colorFolder = gui.addFolder('Color Control')
+    colorFolder.addColor(options, 'background').onChange(col => { scene.background = new THREE.Color(col) })
+    colorFolder.addColor(options, 'object').onChange(col => { absMaterial.color = new THREE.Color(col) })
 }
 
 animate()
